2f28a0ee2aa4a87843cb2dd2b187ff2e783a91c1
tobast <tobast@tobast.fr>
2012-09-15 18:44:50 +0200

    Started 'digital-like' display

---
+++ DigitalNumber.h
@@ -0,0 +1,49 @@
+
+/*
+ * SOFTWARE:
+ *   RubkisTimer
+ * 
+ * AUTHOR:
+ *   Theophile BASTIAN "Tobast" <contact@tobast.fr>
+ *
+ * WEBSITE:
+ *   http://tobast.fr/
+ *
+ * LICENCE:
+ *   GNU GPL v3
+ *
+ * DESCRIPTION:
+ *   Small software designed to act as a Rubik's Cube competition timer.
+ *
+ * LICENCE HEADER:
+ *   Copyright (C) 2012  BASTIAN Theophile
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ */
+
+#ifndef DEF_DIGITALNUMBER
+#define DEF_DIGITALNUMBER
+
+#include <QGraphicsScene>
+#include <QGraphicsItemGroup>
+#include <QGraphicsLineItem>
+#include <QBrush>
+#include <QPen>
+
+class DigitalNumber : public QGraphicsItemGroup
+{
+}
+
+#endif//DEF_DIGITALNUMBER
+
