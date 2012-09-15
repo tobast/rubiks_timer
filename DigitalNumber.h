48120b6eccf12f9c51f7f018d1093a2514592fcb
tobast <tobast@tobast.fr>
2012-09-16 01:23:31 +0200

    corrected the digits matrix

--- DigitalNumber.h
+++ DigitalNumber.h
@@ -58,9 +58,9 @@ static const bool DIGIT_PATTERNS[10][7] =
 	{false,true,true,false,false,true,true},	// 4
 	{true,false,true,true,false,true,true},		// 5
 	{true,false,true,true,true,true,true},		// 6
-	{true,true,true,false,false,false,true},	// 7
+	{true,true,true,false,false,false,false},	// 7
 	{true,true,true,true,true,true,true},		// 8
-	{true,true,true,true,true,false,true}		// 9
+	{true,true,true,true,false,true,true}		// 9
 };
 static const int SEG_HEIGHT=50, SEG_WIDTH=10;
 class DigitalNumber : public QGraphicsItemGroup
