e863988e0f7e608c73f7bc871400514844308e3a
tobast <tobast@tobast.fr>
2012-09-15 16:00:46 +0200

    GraphicalCore added, header created.

---
+++ GraphicalCore.h
@@ -0,0 +1,68 @@
+
+/*
+ * SOFTWARE:
+ *   RubkisTimer
+ * 
+ * AUTHOR:
+ *   Theophile BASTIAN "Tobast" <contact@tobast.fr>
+ *
+ * WEBSITE:
+ *   http://tobast.fr/
+ *
+ * LICENCE:
+ *   GNU GPL v3
+ *
+ * DESCRIPTION:
+ *   Small software designed to act as a Rubik's Cube competition timer.
+ *
+ * LICENCE HEADER:
+ *   Copyright (C) 2012  BASTIAN Theophile
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ */
+
+#ifndef DEF_GRAPHICALCORE
+#define DEF_GRAPHICALCORE
+
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QPushButton>
+#include <QTimer>
+#include <QLabel>
+
+class GraphicalCore : public QWidget
+{
+	Q_OBJECT
+	public:
+		GraphicalCore();
+
+	private slots:
+		void time10ms();
+
+	private://meth
+		void updateDisplay();
+
+	private:
+		QVBoxLayout* l_main;
+		QLabel* timer;
+		QPushButton* resetButton;
+		QLabel* instructions;
+
+		QTimer timer;
+
+		long csecEllapsed; // csed = sec/100
+};
+
+#endif//DEF_GRAPHICALCORE
+
