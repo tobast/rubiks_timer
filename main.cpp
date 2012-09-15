49de571fcb0d33ec8adbb4781ab0070d2d4da14d
tobast <tobast@tobast.fr>
2012-09-15 15:41:45 +0200

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,56 @@
+
+/*
+ * SOFTWARE:
+ *   RubkisTimer
+ * 
+ * AUTHOR:
+ *   Theophile BASTIAN "Tobast" <contact@tobast.fr>
+ *
+ * WEBSITE:
+ *   http://tobast.fr/
+ *
+ * LICENCE:
+ *   GNU GPL v3
+ *
+ * DESCRIPTION:
+ *   Small software designed to act as a Rubik's Cube competition timer.
+ *
+ * LICENCE HEADER:
+ *   Copyright (C) 2012  BASTIAN Theophile
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ */
+
+#include <QApplication>
+#include <QTextCodec>
+#include <QTranslator>
+#include <QLibraryInfo>
+#include <QLocale>
+
+int main(int argc, char** argv)
+{
+	QApplication a(argc, argv);
+	QTextCodec::setCodecForCStrings(QTextCodec::codecForName("utf-8")); // Files are UTF8-encoded
+
+	QString locale = QLocale::system().name().section('_', 0, 0);
+	QTranslator translator;
+	translator.load(QString("qt_") + locale, QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+	a.installTranslator(&translator);
+
+	GraphicalCore* c;
+	c.show();
+	
+	return a.exec();
+}
+
