8477d4b734c61a8d32024710ccf3ffcb46ac42f5
tobast <tobast@tobast.fr>
2012-09-16 14:24:02 +0200

    Filled data in data.h, began AboutDialog.

---
+++ data.h
@@ -0,0 +1,57 @@
+
+/*
+ * SOFTWARE:
+ *   RubkisTimer
+ * 
+ * AUTHOR:
+ *   Theophile BASTIAN "Tobast" <contact@tobast.fr>
+ *
+ * WEBSITE:
+ *   http://tobast.fr/
+ *
+ * LICENCE:
+ *   GNU GPL v3
+ *
+ * DESCRIPTION:
+ *   Small software designed to act as a Rubik's Cube competition timer.
+ *
+ * LICENCE HEADER:
+ *   Copyright (C) 2012  BASTIAN Theophile
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ */
+
+/*
+ * NOTE:
+ *   	According to the terms of the GNU GPLv3 licence, if you choose to modify and redistribute this
+ *   software, you MUST leave anywhere the name of the original developper (eg. in the "about" dialog),
+ *   i.e. Theophile BASTIAN.
+ *		You are not forced, but I would appreciate if you chose to keep my email visible too.
+ *   Also, I would appreciate too if you could in this case send me an email, just to tell me that you've
+ *   forked the project ;)
+ *   	Thanks!
+ */
+
+#ifndef DEF_DATA
+#define DEF_DATA
+
+const char VERSION[] = "v1.0";
+const char AUTHOR[] = "Theophile BASTIAN \"Tobast\"";
+const char CONTACT[] = "contact@tobast.fr";
+const char BUGREP[] = "error-report@tobast.fr";
+const char LICENCE[] = "GNU GPL version 3";
+const char LICENCE_SHORT_TEXT[] = "Copyright (C) 2012 BASTIAN Theophile.\nThis program comes with ABSOLUTELY NO WARRANTY of any kind, and is distributed in the hope that it will be useful. This is free software, and you are welcome to modify it, share and redistribute it under certain conditions. Please refer to the GNU GPL version 3 licence for those conditions.";
+
+#endif//DEF_DATA
+
